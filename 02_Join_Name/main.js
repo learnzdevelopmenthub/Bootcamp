846579c3bcc4d71a464f678e1c17554bd03284f0
learnzdevelopmenthub <learnzdevelopmenthub@gmail.com>
2024-12-03 21:07:29 +0530

    added comment

--- 02_Join_Name/main.js
+++ 02_Join_Name/main.js
@@ -1,6 +1,14 @@
 document.querySelector('#joinBtn').addEventListener('click', ()=>{
     let firstName = document.querySelector('#firstname').value
     let lastName = document.querySelector('#lastname').value
-
+    
     document.querySelector('#result').innerHTML = `${firstName} ${lastName}`
 })
+
+// event handling
+// function, function call
+// ways of event handling
+// varible
+// ways to write funtion
+// + operator
+// template literals
